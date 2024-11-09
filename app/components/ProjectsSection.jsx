929f0b29346571051d00c9d983a5465f8f495e3c
Mohale224 <mohalemalebanye224@gmail.com>
2024-11-09 15:19:10 +0200

    todays-work

--- app/components/ProjectsSection.jsx
+++ app/components/ProjectsSection.jsx
@@ -1,5 +1,7 @@
-import React from 'react';
+"use client";
+import React, { useState } from 'react';
 import ProjectCard from './ProjectCard';
+import ProjectsTag from './ProjectsTag';
 
 const projectsData = [
   
@@ -15,7 +17,7 @@ const projectsData = [
   {
       id: 2,
       title: "Project 2",
-      description: "Agritel",
+      description: "Agritel Contracts Page",
       image: "/image/projects/Agritel1.png",
       tags: ["All","Web"],
       gitUrl: "/",
@@ -24,7 +26,7 @@ const projectsData = [
   {
       id: 3,
       title: "Project 3",
-      description: "Description 3",
+      description: "Animals Tracking Page",
       image: "/image/projects/Agritel2.png",
       tags: ["All","Web"],
       gitUrl: "/",
@@ -33,7 +35,7 @@ const projectsData = [
   {
       id: 4,
       title: "Project 4",
-      description: "Description 4",
+      description: "Plants Tracking Page",
       image: "image/projects/Agritel3.png",
       tags: ["All","Web"],
       gitUrl: "/",
@@ -43,10 +45,35 @@ const projectsData = [
 
 
  const ProjectsSection = () => {
+  const [tag,setTag] = useState("All");
+
+  const handleTagChange = (newtag) => {
+    setTag(newtag);
+  };
+
    return (
      <>
-     <h2>My Projects </h2>
-     <div>{projectsData.map((project) =>(
+     <h2 className='text-4xl font-bold text-white text-center justify-center items-center gap-2  py-6'>My Projects </h2>
+     <div className="text-white flex items-center flex-row">
+      <ProjectsTag 
+      onClick={handleTagChange}
+       name="All"
+        isSelected={tag === "All"}
+       />
+       <ProjectsTag 
+      onClick={handleTagChange}
+       name="Web"
+        isSelected={tag === "Web"}
+       />
+       <ProjectsTag 
+      onClick={handleTagChange}
+       name="Mobile"
+        isSelected={tag === "Mobile"}
+       />
+     </div>
+     <div>
+      
+      {projectsData.map((project) =>(
         <ProjectCard 
           key={project.id} 
           title={project.title}
